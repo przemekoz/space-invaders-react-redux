0a3e4a01f978a2390486afdeca257ca041ec5772
Przemek Kozinski <przemekoz@o2.pl>
2019-11-21 22:58:00 +0100

    added: build version

---
+++ docs/precache-manifest.2759734534d5c6acfac9878f38875eb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a39bb45528a1e8e60457423ab2bdde0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcaf1942b752ec47c9ad",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "d9c86de12b0e8b8aaa25",
+    "url": "/static/js/2.ce99dd48.chunk.js"
+  },
+  {
+    "revision": "bcaf1942b752ec47c9ad",
+    "url": "/static/js/main.0e4b9f73.chunk.js"
+  },
+  {
+    "revision": "efcb4af2decf7fdea261",
+    "url": "/static/js/runtime-main.b0144476.js"
+  }
+]);
