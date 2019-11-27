d8c4007f67bac1f2e90df5220c2c21d55fc73184
Przemek Kozinski <przemekoz@o2.pl>
2019-11-27 07:43:02 +0100

    Updates

---
+++ precache-manifest.8c3807d8f5df2f1508a024a7cb42dded.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ab60d5ad14c54aefae39b97255f481",
+    "url": "/space-invaders-react-redux/index.html"
+  },
+  {
+    "revision": "87917c39ed473f52cf69",
+    "url": "/space-invaders-react-redux/static/css/main.1ac53e6e.chunk.css"
+  },
+  {
+    "revision": "d985df44b69d090582ee",
+    "url": "/space-invaders-react-redux/static/js/2.5d99c025.chunk.js"
+  },
+  {
+    "revision": "87917c39ed473f52cf69",
+    "url": "/space-invaders-react-redux/static/js/main.8972a2a7.chunk.js"
+  },
+  {
+    "revision": "198d3e72d434172e1cbd",
+    "url": "/space-invaders-react-redux/static/js/runtime-main.526a78f1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/space-invaders-react-redux/precache-manifest.8c3807d8f5df2f1508a024a7cb42dded.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/space-invaders-react-redux/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
